2779baa911fc7d7cc70e8e0ae2360458efd3a890
Dani Vicario <danivicario@dvicario.es>
2020-02-27 10:32:01 +0100

    added

--- back/app.js
+++ back/app.js
@@ -52,7 +52,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 const index = require('./routes/index');
-app.use('/', index);
+app.use('/tasks', index);
 
 
 module.exports = app;
--- back/routes/index.js
+++ back/routes/index.js
@@ -1,9 +1,44 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const TodoItem = require("../models/TodoItem");
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+// C (create) of CRUD start
+router.post("/", (req, res, next) => {
+  // we force done to be false; thanks Jota!
+  req.body.done = false;
+
+  TodoItem.create(req.body).then(createdTask => res.json(createdTask));
 });
+// C (create) of CRUD end
+
+// R (read) of CRUD start
+router.get("/all", (req, res, next) => {
+  TodoItem.find()
+    .select({ updatedAt: 0, __v: 0 })
+    .then(allTasks => res.json(allTasks));
+});
+
+router.get("/:id", (req, res, next) => {
+  TodoItem.findById(req.params.id)
+    .select({ updatedAt: 0, __v: 0 })
+    .then(allTasks => res.json(allTasks));
+});
+// R (read) of CRUD end
+
+// U (update) of CRUD start
+router.put("/:id", (req, res, next) => {
+  TodoItem.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(updatedTask =>
+    res.json(updatedTask)
+  );
+});
+// U (update) of CRUD end
+
+// D (update) of CRUD start
+router.delete("/:id", (req, res, next) => {
+  TodoItem.findByIdAndDelete(req.params.id).then(deletedTask =>
+    res.json({ deleted: true, deletedTask })
+  );
+});
+// D (update) of CRUD end
 
 module.exports = router;
