194d368adf55aa1f6950496f11d5ca43d1bf8f5f
Dani Vicario <danivicario@dvicario.es>
2020-02-27 11:51:47 +0100

    added

--- front/src/TodoItem/TodoItem.js
+++ front/src/TodoItem/TodoItem.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./TodoItem.scss";
+import moment from "moment";
 
 export default class TodoItem extends React.Component {
   render() {
     return (
       <section className="TodoItem">
-        <input type="checkbox"/>
-        <h5>Task</h5>
+        <div className="name">
+          <input type="checkbox" />
+          <h5 className="name">{this.props.name}</h5>
+        </div>
+        <div className="actions">
+          <button>⭐️</button>
+          <button>🗑</button>
+          <span>{moment(this.props.createdAt).format("D/MM/YYYY")}</span>
+        </div>
       </section>
     );
   }
--- front/src/TodoList/TodoList.js
+++ front/src/TodoList/TodoList.js
@@ -4,55 +4,84 @@ import "./TodoList.scss";
 import TodoItem from "../TodoItem/TodoItem";
 
 export default class TodoList extends React.Component {
+  state = {
+    todo: [
+      {
+        fav: true,
+        done: false,
+        _id: "5e57882c6972fbfb7547eef4",
+        name: "Task 1",
+        createdAt: "2020-02-27T09:13:16.983Z"
+      },
+      {
+        fav: true,
+        done: false,
+        _id: "5e57882c6972fbfb7547eef5",
+        name: "Task 2",
+        createdAt: "2020-02-27T09:13:16.983Z"
+      },
+      {
+        fav: true,
+        done: false,
+        _id: "5e57882c6972fbfb7547eef6",
+        name: "Task 3",
+        createdAt: "2020-02-27T09:13:16.983Z"
+      }
+    ],
+    done: [
+      {
+        fav: true,
+        done: true,
+        _id: "5e57882c6972fbfb7547eea1",
+        name: "Task 1 done",
+        createdAt: "2020-02-27T09:13:16.983Z"
+      },
+      {
+        fav: true,
+        done: true,
+        _id: "5e57882c6972fbfb7547eea2",
+        name: "Task 2 done",
+        createdAt: "2020-02-27T09:13:16.983Z"
+      },
+      {
+        fav: true,
+        done: true,
+        _id: "5e57882c6972fbfb7547eea3",
+        name: "Task 3 done",
+        createdAt: "2020-02-27T09:13:16.983Z"
+      },
+      {
+        fav: true,
+        done: true,
+        _id: "5e57882c6972fbfb7547eea4",
+        name: "Task 4 done",
+        createdAt: "2020-02-27T09:13:16.983Z"
+      }
+    ]
+  };
   render() {
     return (
       <section className="TodoList">
         <input type="text" placeholder="add new task" id="add-task" />
         <section className="to-do">
-          <h1>To do (4)</h1>
+          <h1>To do ({this.state.todo.length})</h1>
           <ul>
-            <li>
-              <TodoItem></TodoItem>
-            </li>
-            <li>
-              <TodoItem></TodoItem>
-            </li>
-            <li>
-              <TodoItem></TodoItem>
-            </li>
-            <li>
-              <TodoItem></TodoItem>
-            </li>
-            <li>
-              <TodoItem></TodoItem>
-            </li>
-            <li>
-              <TodoItem></TodoItem>
-            </li>
+            {this.state.todo.map(task => (
+              <li>
+                <TodoItem {...task}></TodoItem>
+              </li>
+            ))}
           </ul>
         </section>
 
         <section className="done">
-          <h1>Done (4)</h1>
+          <h1>Done ({this.state.done.length})</h1>
           <ul>
-            <li>
-              <TodoItem></TodoItem>
-            </li>
-            <li>
-              <TodoItem></TodoItem>
-            </li>
-            <li>
-              <TodoItem></TodoItem>
-            </li>
-            <li>
-              <TodoItem></TodoItem>
-            </li>
-            <li>
-              <TodoItem></TodoItem>
-            </li>
-            <li>
-              <TodoItem></TodoItem>
-            </li>
+            {this.state.done.map(task => (
+              <li>
+                <TodoItem {...task}></TodoItem>
+              </li>
+            ))}
           </ul>
         </section>
       </section>
